d8a22bc8a95eabb52db9cb2c055f9d7bee2a5bdc
dryg <xboxseriaskell@gmail.com>
2023-03-01 03:49:13 +0500

    Дз Условные операторы задача 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int clientOSIos = 0;
+        int clientOsAndroid = 1;
+        if ( clientOSIos == 0 && clientOsAndroid ==1 ) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        }
+        System.out.println("Установите версию приложения для Android по ссылке");
     }
 }
