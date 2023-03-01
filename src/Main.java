843766e20fa351a34425b8d99bae8c684c423a75
dryg <xboxseriaskell@gmail.com>
2023-03-01 05:36:47 +0500

    Дз Условные операторы задача 4

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-       int leapYear = 2022;
-       int nonLeapYear = 2021;
-       if (nonLeapYear == 2021) {
-            System.out.println(" 2021 год не являеться весокостным");
-        } if (leapYear == 2022){
-            System.out.println(" 2022 год являеться весокостным");
+        int deliveryDistance = 95;
+        int deliveryFrom1 = 20;
+        int deliveryFrom2 = 50;
+        int deliveryFrom3 = 60;
+        int deliveryFrom4= 0;
+        if (deliveryFrom1 <=20) {
+            System.out.println("Потребуется 1 день,для доставки карты клиенту");
+        }
+        if (deliveryFrom2 ==50 ) {
+            System.out.println("Потребуется 2 дня,для доставки карты клиенту");
+        }
+        if ( deliveryFrom3 >= 60) {
+            System.out.println("Потребуется 3 дня,для доставки карты клиенту");
+        } else if ( deliveryFrom4 ==0) {
+            System.out.println("Доставка свыше 100 км не осуществляеться");
         }
     }
-}
-
-
+    }
